80fee7b190d33cc25716123c35583ef9a6136e7b
Evgeniy-51 <evgeniy-51@yandex.ru>
2023-08-10 01:09:22 +0300

    t1 final

--- JINJA2/t1.py
+++ JINJA2/t1.py
@@ -1,8 +1,23 @@
 from jinja2 import Template
 
-name = "Alex"
-
+user = "Alex"
+age = 34
 tm = Template("Hello {{ name }}")
-msg = tm.render(name=name)
-
+msg = tm.render(name=user)
 print(msg)
+
+tm = Template("My name is {{ a.upper() }}. I'm {{ b+2 }}")
+msg = tm.render(a=user, b=age)
+print(msg)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person('Ivan', 44)
+tm = Template("My name is {{ a.name }}. I'm {{ a.age }}")
+msg = tm.render(a=p1)
+print(msg)
+
+
