9dcd6b8893af3aaf2b6a7bcc83495ed7d8f05cba
Evgeniy-51 <evgeniy-51@yandex.ru>
2023-08-10 19:23:33 +0300

    t4 added

--- JINJA2/t4_macro_call.py
+++ JINJA2/t4_macro_call.py
@@ -13,4 +13,29 @@ tm = Template(html)
 msg = tm.render()
 print(msg)
 
+cars = [
+    {'model': 'Audi', 'price': 5_000_000},
+    {'model': 'Volvo', 'price': 4_500_000},
+    {'model': 'Kia', 'price': 2_200_000},
+    {'model': 'Lada', 'price': 600_000},
+]
 
+html = """
+{% macro list_cars(list_of_cars) -%}
+<ul>
+{%- for i in list_of_cars %}
+    <li>{{i.model.upper()}} {{caller(i)}}<li>
+{%- endfor %}
+<ul>
+{%- endmacro %}
+
+{% call(car) list_cars(cars) %}
+    <ul>
+        <li>Model: {{car.model}}
+        <li>Price: {{car.price}}
+    <ul>
+{% endcall -%}
+"""
+tm = Template(html)
+msg = tm.render(cars=cars)
+print(msg)
