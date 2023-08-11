08b90d45c078c641af3d33a72574f8c33a59ed21
Evgeniy-51 <evgeniy-51@yandex.ru>
2023-08-12 00:44:46 +0300

    add tf_5

--- FLASK/tf2_render_template.py
+++ FLASK/tf2_render_template.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+menu = ["Log In", "Setting", "About"]
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="Flask lesson", menu=menu)
 
 
 if __name__ == '__main__':
