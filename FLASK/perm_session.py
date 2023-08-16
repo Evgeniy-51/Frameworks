75b346cf1c90d08439822d841cc4a78f8f2622ae
Evgeniy-51 <evgeniy-51@yandex.ru>
2023-08-16 20:46:06 +0300

    finish tf_5

---
+++ FLASK/perm_session.py
@@ -0,0 +1,19 @@
+from  flask import Flask, request, render_template, redirect, url_for
+
+app= Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("perm_sess_login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h>{usr}</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
