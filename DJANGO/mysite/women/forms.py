c8fedb4a1cbbb17d48f0f2d3000121483fdfb90f
Evgeniy-51 <evgeniy-51@yandex.ru>
2023-09-19 01:25:47 +0300

    add forms

--- DJANGO/mysite/women/forms.py
+++ DJANGO/mysite/women/forms.py
@@ -3,8 +3,8 @@ from .models import *
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
-    is_pubished = forms.BooleanField()
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=255, label="Заголовок")
+    slug = forms.SlugField(max_length=255, label="URL")
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label="Контент")
+    is_pubished = forms.BooleanField(required=False, initial=True)
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категории", empty_label="Категория не выбрана")
--- DJANGO/mysite/women/models.py
+++ DJANGO/mysite/women/models.py
@@ -4,12 +4,12 @@ from django.urls import reverse
 
 class Women(models.Model):  # 1 экземпляр == 1 строка БД
     # id  создается по дефолту
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=False)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    content = models.TextField(blank=False, verbose_name="Содержание статьи")
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name="Фото")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
@@ -18,10 +18,15 @@ class Women(models.Model):  # 1 экземпляр == 1 строка БД
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = "Известные женщины"
+        verbose_name_plural = "Известные женщины"
+        ordering = ['-time_create', 'title']
+
 
 class Category(models.Model):
     objects = None
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
 
     def __str__(self):
         return self.name
@@ -29,6 +34,10 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
 
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['name']
 
 # class User(models.Model):
 #     name = models.CharField(max_length=100)
